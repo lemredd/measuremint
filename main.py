e8fb399716bd1af5d6b25908ac9b064fce76328e
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2025-05-17 11:15:47 +0800

    feat(main): prepare static serving and templates

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
+templates = Jinja2Templates(directory="html")
