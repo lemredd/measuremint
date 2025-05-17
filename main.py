345e981aa4dc27e82ab72e4ea4ab88f734520283
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2025-05-17 12:42:31 +0800

    feat(main): filter suggestions by given value

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
+from pint import UnitRegistry
+
 app = FastAPI()
 
 templates = Jinja2Templates(directory="html")
@@ -11,6 +13,22 @@ async def index(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
 
 
+units = sorted([str(unit).replace("_", " ") for unit in UnitRegistry()])
+
+
 @app.get("/suggestions")
 async def suggest_units(request: Request):
-    return templates.TemplateResponse(request=request, name="suggestions.html")
+    value = request.query_params.get("from") or request.query_params.get("to")
+
+    if value:
+        filtered = [*filter(lambda u: value in u, units)][:10]
+    else:
+        filtered = units[:10]
+
+    if not len(filtered):
+        filtered = ["No units found"]
+
+    context = {"units": filtered}
+    return templates.TemplateResponse(
+        request=request, name="suggestions.html", context=context
+    )
