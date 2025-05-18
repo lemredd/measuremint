5114aadcea40ee1240951812569090246c713cad
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2025-05-18 19:19:11 +0800

    docs(main): document `/hx` routes minimally

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from decimal import Decimal, ROUND_UP
 from typing import Annotated
 
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI, Request, Response, Form
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request, Response, Form, Query
 
 from pint import UnitRegistry
 from pint.errors import DimensionalityError, UndefinedUnitError
@@ -14,14 +16,27 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="html")
 
+units = sorted([str(unit).replace("_", " ") for unit in UnitRegistry()])
+
 
 @app.get("/")
 async def index(request: Request) -> Response:
     return templates.TemplateResponse(request=request, name="index.html")
 
 
-hx = FastAPI()
-units = sorted([str(unit).replace("_", " ") for unit in UnitRegistry()])
+hx_description = """You would not usually access this API directly.
+All of the documentation below are read-only.
+The use of `HX-Request` header is also **required** for requests to work.
+\nTo see the web app in action, visit the [homepage](/)."""
+hx = FastAPI(
+    title="MeasureMint HTMX API",
+    summary="MeasureMint's HTMX API Documentation. For developer use only.",
+    description=hx_description,
+    # TODO: get latest git tag
+    version="0.1.0",
+    swagger_ui_parameters={"supportedSubmitMethods": []},
+    redoc_url=None,
+)
 
 
 @hx.middleware("http")
@@ -35,10 +50,18 @@ async def check_hx_request(request: Request, call_next):
     return Response("Cannot process this request", status_code=403)
 
 
-@hx.get("/suggestions")
-async def suggest_units(request: Request) -> Response:
-    value = request.query_params.get(
-        "from_unit") or request.query_params.get("to_unit")
+@hx.get("/suggestions", response_class=HTMLResponse)
+async def suggest_units(
+    request: Request,
+    from_unit: Annotated[str | None, Query()] = None,
+    to_unit: Annotated[str | None, Query()] = None,
+) -> HTMLResponse:
+    """
+    Returns a list of units that match the query.
+    List of units come from [pint](https://pint.readthedocs.io/en/stable/).
+    \nThese are used by the unit inputs in the main form.
+    """
+    value = from_unit or to_unit
 
     if value:
         filtered = [*filter(lambda u: value in u, units)][:10]
@@ -54,13 +77,18 @@ async def suggest_units(request: Request) -> Response:
     )
 
 
-@hx.post("/convert")
+@hx.post("/convert", response_class=HTMLResponse)
 async def convert(
     request: Request,
     quantity: Annotated[float, Form()],
     from_unit: Annotated[str, Form()],
     to_unit: Annotated[str, Form()],
-) -> Response:
+) -> HTMLResponse:
+    """
+    Converts `quantity` from `from_unit` to `to_unit`.
+    validation of units is handled by [pint](https://pint.readthedocs.io/en/stable/).
+    \nThis takes action when the form is submitted.
+    """
     from_unit = from_unit.replace(" ", "_")
     to_unit = to_unit.replace(" ", "_")
 
